55a4ab6c59999a990b6740b4c80457ca6e150a33
Nickolay Samovendyuk <samovnick@gmail.com>
2019-10-07 10:41:18 +0300

    feat: implement task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,30 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.min = 0;
+        this.max = 100;
+        this.current = 50;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
+        this.current = Math.round((this.max - this.min) / 2) + this.min;
     }
 
     guess() {
+        
+        return this.current
 
     }
 
     lower() {
-
+        this.max = this.current;
+        this.current = Math.round((this.max - this.min) / 2) + this.min;
     }
 
     greater() {
-
+        this.min = this.current;
+        this.current = Math.round((this.max - this.min) / 2) + this.min;
     }
 }
 
